a9d57bd5ee53d1ccc8ba7c21e7486768677c9dbb
dexcription <92596419+dexcription@users.noreply.github.com>
2026-08-26 18:36:35 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'dexcription'
+description 'A collection of optimized Lua helper functions for FiveM'
+version '1.0.0'
+
+shared_scripts {
+    'shared/helpers.lua'
+}
